195ff07f9aeac88b8b418cf7559f973f606f5b9f
marcociama <marco.ciamarra0@gmail.com>
2026-02-09 22:35:10 +0100

    prova 1 tsDetect

--- src/test/java/CalcolatriceTest.java
+++ src/test/java/CalcolatriceTest.java
@@ -1,10 +1,15 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalcolatriceTest {
+public class CalcolatriceTest {
     @Test
-    void testSomma() {
+    public void testSomma() {
         Calcolatrice calc = new Calcolatrice();
         assertEquals(5, calc.somma(2, 3));
+    }
+
+    @Test
+    public void vuoto(){
+
     }
 }
